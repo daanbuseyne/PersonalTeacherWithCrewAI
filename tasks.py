c4f8e4c48ffef4ab94c1fb28b5e8a0a157983308
daanbuseyne <47984515+daanbuseyne@users.noreply.github.com>
2024-09-03 13:49:15 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -2,9 +2,7 @@ from crewai import Task
 from textwrap import dedent
 
 
-# This is an example of how to define custom tasks.
-# You can define as many tasks as you want.
-# You can also define custom agents in agents.py
+
 class CustomTasks:
     def __tip_section(self):
         return "If you do your BEST WORK, you will have happy students!"
